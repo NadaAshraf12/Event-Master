719f149945be76c772fb46e801d46e1d00c90f65
NadaAshraf12 <nadana12155@gmail.com>
2025-04-16 18:23:54 +0300

    Initial commit

---
+++ src/components/Events.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Events = (props) => {
+  return (
+    <div id="Events" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Events</h2>
+          <p>
+          Explore and join exciting events happening near you!
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
